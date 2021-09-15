b6919d5ec2d3ada47e1fdb59a197907b3b4f469c
Rob Spoon <Remspoon1@Gmail.com>
2021-09-15 13:22:13 -0400

    places Reviews endpoints testing

--- packages/server/src/routes/places.js
+++ packages/server/src/routes/places.js
@@ -1,17 +1,45 @@
 import express from 'express'
-import { Places } from '../models'
+import { Place, Review} from '../models'
 
 const router = express.Router()
 
-router.put("/", async (req,res,next)=>{
 
-    let place = await Place.findOne({}).exec()
-
-    if (!place) {
-        const newPlace = new Place({
-          username: "Freddie",
-        })
-        place = await newPlace.save()
-      }
-
+//get PLACE
+router.get("/:id", async (req,res) => {
+    let place = await Place.findById(req.params.id)
+    if (place){
+        res.json(place.json())
+    } else {
+        res.status(400).end
+    }
 })
+
+//get REVIEW
+router.get("/:id", async(req,res) => {
+    let review = await Review.findbyId(req.params.id)
+    if (review) {
+        res.json(review.json())
+    } else {  
+        res.status(400).end
+    }
+})
+
+//post REVIEW
+router.post("/", requireauth, async (req,res)=>{
+    let {text} = request.body
+    let {user} = request
+
+    const review = new Review({
+        text:text,
+        author: user._id,
+    })
+    let savedReview = await review.save()
+
+    if (savedReview) {
+        res.json(savedReview.tojson())
+    } else {
+        res.status(400).end
+    }
+})
+   
+module.exports = router
