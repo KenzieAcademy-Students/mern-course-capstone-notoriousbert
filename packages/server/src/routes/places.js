9c4deb3a6aa54fdd00763a27294e7aecfcfa296e
Vic <YOUREMAIL@YOUREMAIL.com>
2021-09-20 14:06:32 -0600

    post endpoint finished

--- packages/server/src/routes/places.js
+++ packages/server/src/routes/places.js
@@ -1,4 +1,30 @@
 import express from "express";
+import { Place } from "../models";
 const router = express.Router();
+
+
+router.post("/", async (request, response, next) => {
+  const { typeOfPlace, placeName, address, aptOrSuitNumber,city, state, zipcode, pricePerNight, petsAllowed } = request.body;
+  const place = new Place({
+    typeOfPlace: typeOfPlace,
+    placeName: placeName,
+    address: address,
+    aptOrSuitNumber: aptOrSuitNumber,
+    city: city,
+    state: state,
+    zipcode: zipcode,
+    pricePerNight: pricePerNight,
+    petsAllowed: petsAllowed,
+
+  });
+
+  try {
+    const savedPlace = await place.save();
+    response.json(savedPlace);
+  } catch (error) {
+    next(error);
+  }
+});
+
 
 module.exports = router;
