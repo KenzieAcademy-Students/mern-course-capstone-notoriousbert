37baf71d90ae1e834da62499e45cd63b7d8d99b8
Rob Spoon <Remspoon1@Gmail.com>
2021-09-20 16:58:27 -0400

    added bootstrap dependency

--- packages/client/src/index.js
+++ packages/client/src/index.js
@@ -2,13 +2,15 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import { BrowserRouter, Route, Switch } from 'react-router-dom'
 import HomePage from 'pages/HomePage'
+import LandingPage from 'pages/LandingPage'
 import './index.css'
+import 'bootstrap/dist/css/bootstrap.min.css'
 
 ReactDOM.render(
   <React.StrictMode>
     <BrowserRouter>  
       <Switch>
-        <Route exact path='/' component={HomePage} />
+        <Route exact path='/' component={LandingPage} />
         { /* Add more routes here */} 
       </Switch>
     </BrowserRouter>
--- packages/client/src/pages/LandingPage.js
+++ packages/client/src/pages/LandingPage.js
@@ -1,1 +1,24 @@
+import React from 'react'
+import { Container } from 'react-bootstrap'
+import { Row } from 'react-bootstrap'
+import { Col, Button } from 'react-bootstrap'
 
+export default function LandingPage(props) {
+return (
+    <div>
+    <Container fluid>
+        <Row>
+            {/* <p> Row one </p> */}
+            <Col>
+            <p> Col One</p>
+            </Col>
+        </Row>
+        <Row>
+            {/* <p> Row Two </p> */}
+            <Col> One - About K/R </Col>
+            <Col> Two - Our Mission </Col>
+            <Col> Three - Contact US</Col>
+        </Row>
+    </Container>
+    </div>
+)}
