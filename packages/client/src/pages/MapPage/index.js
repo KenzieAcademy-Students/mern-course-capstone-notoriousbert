c32f7595a31f57f2abe9872ff345eba85462ed9c
Albert Meuse <albertmeuse22@gmail.com>
2021-09-23 10:16:29 -0700

    added search toolbar, navigator that centers map on user location, and popup windows when a marker is clicked on

--- packages/client/src/pages/MapPage/index.js
+++ packages/client/src/pages/MapPage/index.js
@@ -2,7 +2,6 @@ import React from "react";
 import mapStyles from "./mapStyles";
 import "./index.scss";
 import { useState, useCallback, useRef } from "react";
-// import { GoogleMap, withScriptjs, withGoogleMap } from "react-google-maps";
 
 import {
   GoogleMap,
@@ -12,31 +11,18 @@ import {
 } from "@react-google-maps/api";
 import { formatRelative } from "date-fns";
 
-// import usePlacesAutocomplete, {
-//     getGeocode,
-//     getLatLng,
-// } from "use-places-autocomplete"
-// import {
-//     Combobox,
-//     ComboboxInput,
-//     ComboboxPopover,
-//     ComboboxList,
-//     ComboboxOption,
-// } from "@reach/combobox"
+import usePlacesAutocomplete, {
+  getGeocode,
+  getLatLng,
+} from "use-places-autocomplete";
+import {
+  Combobox,
+  ComboboxInput,
+  ComboboxPopover,
+  ComboboxList,
+  ComboboxOption,
+} from "@reach/combobox";
 import "@reach/combobox/styles.css";
-
-console.log(process.env);
-
-// function Map() {
-//   return (
-//     <GoogleMap
-//       defaultZoom={8}
-//       defaultCenter={{ lat: 45.421532, lng: -75.697189 }}
-//     />
-//   );
-// }
-
-// const WrappedMap = withScriptjs(withGoogleMap(Map));
 
 const libraries = ["places"];
 
@@ -63,22 +49,28 @@ export default function MapPage() {
   });
 
   const [markers, setMarkers] = useState([]);
+  const [selected, setSelected] = useState(null);
 
   const onMapClick = useCallback((event) => {
-      setMarkers((current) => [
-        ...current,
-        {
-          lat: event.latLng.lat(),
-          lng: event.latLng.lng(),
-          time: new Date(),
-        },
-      ]);
-  }, [])
+    setMarkers((current) => [
+      ...current,
+      {
+        lat: event.latLng.lat(),
+        lng: event.latLng.lng(),
+        time: new Date(),
+      },
+    ]);
+  }, []);
 
-  const mapRef =useRef()
+  const mapRef = useRef();
   const onMapLoad = useCallback((map) => {
-    mapRef.current = map
-  }, [])
+    mapRef.current = map;
+  }, []);
+
+  const panTo = useCallback(({ lat, lng }) => {
+    mapRef.current.panTo({ lat, lng });
+    mapRef.current.setZoom(14);
+  }, []);
 
   if (loadError) return "Error loading maps";
   if (!isLoaded) return "Loading Maps";
@@ -91,26 +83,121 @@ export default function MapPage() {
           🐩 🐈 🐦 🦎
         </span> */}
       </h1>
+
+      <Search panTo={panTo} />
+      <Locate panTo={panTo} />
+
       <GoogleMap
         mapContainerStyle={mapContainerStyle}
         zoom={8}
         center={center}
         options={options}
         onClick={onMapClick}
+        onLoad={onMapLoad}
       >
         {markers.map((marker) => (
           <Marker
             key={marker.time.toISOString()}
             position={{ lat: marker.lat, lng: marker.lng }}
             icon={{
-              url: '/icons8-dog.svg',
+              url: "/icons8-dog.svg",
               scaledSize: new window.google.maps.Size(30, 30),
               origin: new window.google.maps.Point(0, 0),
-              anchor: new window.google.maps.Point(15, 15)
+              anchor: new window.google.maps.Point(15, 15),
+            }}
+            onClick={() => {
+              setSelected(marker);
             }}
           />
         ))}
+
+        {selected ? (
+          <InfoWindow
+            position={{ lat: selected.lat, lng: selected.lng }}
+            onCloseClick={() => {
+              setSelected(null);
+            }}
+          >
+            <div>
+              <h2>Place Name</h2>
+              <h3>Pets Allowed: </h3>
+              <p>{formatRelative(selected.time, new Date())}</p>
+            </div>
+          </InfoWindow>
+        ) : null}
       </GoogleMap>
+    </div>
+  );
+}
+
+function Locate({ panTo }) {
+  return (
+    <button
+      className="locate"
+      onClick={() => {
+        navigator.geolocation.getCurrentPosition(
+          (position) => {
+            panTo({
+              lat: position.coords.latitude,
+              lng: position.coords.longitude,
+            })
+          },
+          () => null
+        );
+      }}
+    >
+      <img src="icons8-compass.svg" alt="compass - locate me" />
+    </button>
+  );
+}
+
+function Search({ panTo }) {
+  const {
+    ready,
+    value,
+    suggestions: { status, data },
+    setValue,
+    clearSuggestions,
+  } = usePlacesAutocomplete({
+    requestOptions: {
+      location: { lat: () => 43.653225, lng: () => -79.383186 },
+      radius: 200 * 1000,
+    },
+  });
+
+  return (
+    <div className="search">
+      <Combobox
+        onSelect={async (address) => {
+          setValue(address, false);
+          clearSuggestions();
+
+          try {
+            const results = await getGeocode({ address });
+            const { lat, lng } = await getLatLng(results[0]);
+            panTo({ lat, lng });
+          } catch (error) {
+            console.log("error finding address.");
+          }
+        }}
+      >
+        <ComboboxInput
+          value={value}
+          onChange={(event) => {
+            setValue(event.target.value);
+          }}
+          disabled={!ready}
+          placeholder="Enter a place"
+        />
+        <ComboboxPopover>
+          <ComboboxList>
+            {status === "OK" &&
+              data.map(({ id, description }) => (
+                <ComboboxOption key={id} value={description} />
+              ))}
+          </ComboboxList>
+        </ComboboxPopover>
+      </Combobox>
     </div>
   );
 }
