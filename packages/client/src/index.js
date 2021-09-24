a45d825c222e3777735bb0e9df86d1b02c2b2a40
alicialwilliams <63070379+alicialwilliams@users.noreply.github.com>
2021-09-24 14:23:51 -0400

    added dependencies

--- packages/client/src/index.js
+++ packages/client/src/index.js
@@ -21,11 +21,8 @@ ReactDOM.render(
     <Navbar />
       <Switch>
         <Route exact path='/' component={LandingPage} />
-<<<<<<< HEAD
         <Route exact path='/places/placeId'component={PlacesDetailPage}   />
-=======
         <Route exact path='/map' component={MapPage} />
->>>>>>> 51f590c97c0407578ebda00cb9799a7ffeecf9e6
         <Route exact path='/signup' component={UserRegistrationPage} />
         <Route exact path='/login' component={LoginPage} />
       </Switch>
