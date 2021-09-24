20a79ef3c64ffa243e3c3f7032dc1652ef458773
alicialwilliams <63070379+alicialwilliams@users.noreply.github.com>
2021-09-24 15:26:06 -0400

    deleted info from placesDetailsPage

--- packages/client/src/pages/PlacesDetailPage.js
+++ packages/client/src/pages/PlacesDetailPage.js
@@ -1,23 +1,6 @@
-import React, { useState, useEffect } from 'react'
-import Axios from 'axios';
-import { Container } from 'react-bootstrap'
+import React from 'react'
 
-export default function PlacesDetailPage({
-match:{
-    params: {placeId},
-},
-
-}) {
-    const [place, setPlace] = useState([]);
-
-    useEffect(()=>{
-         Axios.get(`api/place/places_by_id?id=${placeId}&type=single`)
-         .then(response =>{
-             setPlace(response.data[0])
-
-         })
-    },[])
-
+export default function PlacesDetailPage() {
     return (
         <div>
             
