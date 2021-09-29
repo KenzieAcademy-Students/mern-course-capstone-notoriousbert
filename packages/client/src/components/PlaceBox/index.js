c2810af4437b3c17cfbdc327ba4189b7695e3515
alicialwilliams <63070379+alicialwilliams@users.noreply.github.com>
2021-09-29 15:51:42 -0400

    put placeDetails and reviews in two separate rows.

--- packages/client/src/components/PlaceBox/index.js
+++ packages/client/src/components/PlaceBox/index.js
@@ -1,5 +1,5 @@
 import React, { useEffect } from 'react'
-import { Container } from 'react-bootstrap'
+import { Container, Row, Col } from 'react-bootstrap'
 
 
 
@@ -10,28 +10,48 @@ const PlaceBox = ({ mapMarker }) => {
   }, [])
   return (
     <Container className='p-sm-2 p-md-4'>
-      <div className='row justify-content-left mb-2'>
-        <div className='col-xs-8 col-md-7'>
-          <h1>{mapMarker.placeName}</h1>
-        </div>
-      </div>
-      <div className='row h-25 justify-content-left'>
-        <div className='col-xs-8 col-md-7'>
-          <div className='row h-25'>
-            <div className='col-6 align-self-left'>
-              <h4>{mapMarker.typeofPlace}</h4>
-            </div>
-            <div className='col-6 align-self-left text-right mb-2'>
-              <div className='text-secondary'>
-                <h3>{mapMarker.address}, {mapMarker.city}, {mapMarker.state}, {mapMarker.zipcode}</h3>
-              </div>
-            </div>
-            <div className='col-12 align-self-center mb-3'>
-              <h4>Pet's Allowed: {mapMarker.petsAllowed.map((pet, index) => (index === mapMarker.petsAllowed.length - 1 ? <>{pet.category}</> : <>{pet.category}, </>))} </h4>
+      <Row>
+        <Col>
+          <div className='row justify-content-left mb-2'>
+            <div className='col-xs-8 col-md-7'>
+              <h1>{mapMarker.placeName}</h1>
             </div>
           </div>
-        </div>
-      </div>
+          <div className='row justify-content-left mb-2'>
+            <div className='col-xs-8 col-md-7'>
+              <h4>{mapMarker.typeofPlace}</h4>
+            </div>
+          </div>
+          <div className='row justify-content-left mb-2'>
+            <div className='col-xs-8 col-md-7'>
+              <h3>{mapMarker.address} <br /> {mapMarker.city},{mapMarker.state} <br />{mapMarker.zipcode}</h3>
+            </div>
+          </div>
+          <div className='row justify-content-left mb-2'>
+            <div className='col-xs-8 col-md-7'>
+              <h6>Pet's Allowed: {mapMarker.petsAllowed.map((pet, index) => (index === mapMarker.petsAllowed.length - 1 ? <>{pet.category}</> : <>{pet.category}, </>))} </h6>
+            </div>
+          </div>
+        </Col>
+      </Row>
+      <Row>
+        <Col>
+          <div className='row justify-content-left mb-2'>
+            <div className='col-xs-8 col-md-7'>
+              <h2>Reviews <br />{mapMarker.reviews.map((review) => <div>
+                <h4>
+                  {review.author.username} - {review.created}
+                </h4>
+                <div>
+                  {review.text}
+                </div>
+              </div>)} </h2>
+            </div>
+          </div>
+
+        </Col>
+      </Row>
+
 
     </Container>
   )
