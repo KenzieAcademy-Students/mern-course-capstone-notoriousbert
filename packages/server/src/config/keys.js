e360899f724dc25621d9753a404dc4a15f409be6
Albert Meuse <albertmeuse22@gmail.com>
2021-10-12 14:47:39 -0700

    changed the config url to have or in it

--- packages/server/src/config/keys.js
+++ packages/server/src/config/keys.js
@@ -1,14 +1,16 @@
 module.exports = {
   app: {
     name: "MERN App",
-    apiEndpoint: (process.env.API_URL) ? `/${process.env.API_URL}` : '/api',
+    apiEndpoint: process.env.API_URL ? `/${process.env.API_URL}` : "/api",
   },
   database: {
-    // url: process.env.MONGODB_URI || 'mongodb://localhost:27017/MERNApp', // for local mongodb
-    url: "mongodb+srv://CassandraT123:CassandraT123@kibblesandritz.mvb2v.mongodb.net/sample_dataset?retryWrites=true&w=majority" // for mongodb atlas, comment the above line, uncomment this line, and add your connection string.
+    url:
+      process.env.MONGODB_URI ||
+      "mongodb+srv://CassandraT123:CassandraT123@kibblesandritz.mvb2v.mongodb.net/sample_dataset?retryWrites=true&w=majority", // for local mongodb
+    // url: "mongodb+srv://CassandraT123:CassandraT123@kibblesandritz.mvb2v.mongodb.net/sample_dataset?retryWrites=true&w=majority" // for mongodb atlas, comment the above line, uncomment this line, and add your connection string.
   },
   jwt: {
-    secret: process.env.JWT_SECRET || 'jwt-secret',
-    tokenLife: '7d',
+    secret: process.env.JWT_SECRET || "jwt-secret",
+    tokenLife: "7d",
   },
-}
+};
