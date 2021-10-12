5739417d5811bb26ab90b1f64fe6c721efa697f7
Albert Meuse <albertmeuse22@gmail.com>
2021-10-12 14:35:26 -0700

    changed line 37 into client/build

--- packages/server/src/app.js
+++ packages/server/src/app.js
@@ -34,7 +34,7 @@ app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, "public")));
+app.use(express.static(path.join(__dirname, "/client/build")));
 app.use(requestLogger);
 
 app.use(express.static(path.join(__dirname, "/client/build/index.html")));
