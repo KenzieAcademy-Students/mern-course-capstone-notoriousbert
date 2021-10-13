5d4670e18970d2b5c2c9d3f93bd41618a5b55723
Albert Meuse <albertmeuse22@gmail.com>
2021-10-12 20:27:34 -0700

    changed from plus to comma

--- packages/server/src/app.js
+++ packages/server/src/app.js
@@ -48,7 +48,7 @@ if (process.env.NODE_ENV === "production") {
   // app.use(express.static("client/build"));
   // app.use(express.static(path.join(__dirname, "../../client/build")));
   app.get("*", function (req, res) {
-    res.sendFile(path.join(__dirname + "../../client/build/index.html"));
+    res.sendFile(path.join(__dirname, "../../client/build/index.html"));
   });
 }
 
