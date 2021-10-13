66c1ad482095d182e8660a1604096efebcab3eb2
Albert Meuse <albertmeuse22@gmail.com>
2021-10-12 18:34:36 -0700

    changed the express.static pathing

--- packages/server/src/app.js
+++ packages/server/src/app.js
@@ -32,7 +32,8 @@ const app = express();
 if (process.env.NODE_ENV === "production") {
   console.log(__dirname);
   console.log("PRODUCTION PRODUCTION");
-  app.use(express.static("client/build"));
+  // app.use(express.static("client/build"));
+  app.use(express.static(path.join(__dirname, "public")));
   app.get("*", function (req, res) {
     res.sendFile(path.join(__dirname, "../../client/build/index.html"));
   });
